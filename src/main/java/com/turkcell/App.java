509fbf023922af425418a306a0f8469443651b53
berkyildiz52 <berkyildiz5299@gmail.com>
2024-04-24 19:55:56 +0300

    core update many things again

--- src/main/java/com/turkcell/App.java
+++ src/main/java/com/turkcell/App.java
@@ -1,12 +1,14 @@
 package com.turkcell;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+public class App
 {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- src/main/java/com/turkcell/filters/JwtAuthFilter.java
+++ src/main/java/com/turkcell/filters/JwtAuthFilter.java
@@ -1,21 +1,20 @@
 package com.turkcell.filters;
 
 import com.turkcell.jwt.JwtService;
-import lombok.RequiredArgsConstructor;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
